b17d49c2752ae557d4332a089ec4792aae0deaa1
harshit-jain52 <harshitjain0502@gmail.com>
2024-09-27 19:06:57 +0530

    test(tinyC2): Detailed Test File

--- TinyC2/input_22CS10030_22CS10049.c
+++ TinyC2/input_22CS10030_22CS10049.c
@@ -1,9 +1,91 @@
-int foo(int x){
-    return sizeof(x);
+/* Test File */
+
+// External Declarations
+extern int globe;
+static double PI = 3.14159;
+const long INF = 1e9;
+
+// Function prototype
+_Bool bool_foo(char *val);
+
+// Function definition
+inline void newline(){
+    auto char* str = "Customized newline function";
+    printf("%s\n", str);
 }
 
-int main(){
-    int a;
-    foo(a);
-    return 0;
+int main()
+{   
+    // Declaration statements
+    long int x = 10, iter = 0;
+    float f = 12.34e-5 + 90;
+    char c = '$';
+    char str[] = "Hello, Tiny World!";
+    int arr[5] = {2, 3, 5, 7, 11};
+    register int fast_var = 50;
+
+    // Assignment Expression
+    x += 5;
+    fast_var *= 2;
+
+    // Logical Expression
+    _Bool flag = (x > 5 && fast_var < 100) || (x != INF);
+
+    // Iteration Statements
+    do {
+        printf("Iteration %ld\n", iter);
+    } while(iter++ < 5);
+
+    while(iter>0) iter--;
+
+    for(int i=0; ;i++){
+        if(i==1) continue;
+        if(i==10) break;
+    }
+
+    // Cast Expression
+    double a = (double)x/2;
+
+    // Unary Expression
+    double* ptr = &a;
+    double val = *ptr;
+
+    // Multiplicative and Additive Expression
+    int res2 = val*2 + val/2 - x%5;
+
+    // Shift Expression
+    x = (x<<3)>>2;
+    
+    // Function Call
+    _Bool err = bool_foo(str);
+
+    // Selection Statements
+    switch(x) {
+        case 5:
+            printf("x is 5\n");
+            break;
+        case 15:
+            printf("x is 15\n");
+            break;
+        default:
+            printf("x is something else\n");
+            break;
+    }
+
+    // Jump Statement
+    if(err) {
+        goto exit;
+    } else {
+        printf("No error\n");
+    }
+    
+    // Labelled Statement
+    exit:
+        return 0;
 }
+
+/*
+End
+Of
+File
+*/
