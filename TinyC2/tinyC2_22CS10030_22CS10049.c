9328c8c0c219bdc721cf6e58885d3a12fa3a9dd5
Nived04 <shahnived04@gmail.com>
2024-09-30 14:08:13 +0530

    docx (TinyC2): added comments

--- TinyC2/tinyC2_22CS10030_22CS10049.c
+++ TinyC2/tinyC2_22CS10030_22CS10049.c
@@ -5,6 +5,8 @@ void throw_error(char *err){
     fprintf(stderr, "***Error at line %d: %s\n", yylineno, err);
 }
 
+// function to create node of the parse tree, with the production to output 
+// and the number of children: using variadic functions
 parse_tree_node* create_node(char* production_text, int num_children, ...) {
 	parse_tree_node* new_node = (parse_tree_node*)malloc(sizeof(parse_tree_node));
 	new_node->text = strdup(production_text);
@@ -12,11 +14,14 @@ parse_tree_node* create_node(char* production_text, int num_children, ...) {
 
 	if(!num_children) return new_node;
 
-	va_list args;
-	va_start(args, num_children);
+	va_list args; // for handling variable number of children
+	va_start(args, num_children); // initialize the va_list
 	new_node->children = add_child_node(va_arg(args, parse_tree_node*));
 	node_child_list* mover = new_node->children;
 	int ct = 1;
+
+	// va_arg updates the va_list to the next argument
+
 	while(ct < num_children){
 		mover->next = add_child_node(va_arg(args, parse_tree_node*));
 		mover = mover->next;
@@ -26,6 +31,7 @@ parse_tree_node* create_node(char* production_text, int num_children, ...) {
 	return new_node;
 }
 
+// function to add a node to the linked list of children of a node
 node_child_list* add_child_node(parse_tree_node* data){
 	node_child_list* temp = (node_child_list*)malloc(sizeof(node_child_list));
 	temp->child = data;
@@ -33,6 +39,7 @@ node_child_list* add_child_node(parse_tree_node* data){
 	return temp;
 }
 
+// frees the node memory of the parse tree
 void clean_parse_tree(parse_tree_node* root){
 	if(root == NULL) return;
 	node_child_list* mover = root->children;
@@ -46,6 +53,7 @@ void clean_parse_tree(parse_tree_node* root){
 	free(root);
 }
 
+// prints the production in NLR format (LR representing left to right child printing)
 void print_productions(parse_tree_node* root, int level){
 	if(root == NULL) return;
 	print_spaces(level);
@@ -57,6 +65,7 @@ void print_productions(parse_tree_node* root, int level){
 	}
 }
 
+// spaces for tree printing
 void print_spaces(int num){
     for(int i = 0; i < num; i++) printf("  ");
 }
